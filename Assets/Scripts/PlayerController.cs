c3606dba8eeee30af740f0fdc96a420578fa9d09
habibi55 <muhamadhabibi14168@gmail.com>
2021-10-20 16:47:12 +0700

    problem-2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D playerRigidbody;
+    private float playerSpeed = 10f;
+
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveTo();
+    }
+
+    // menggerakkan pemain ke arah vector (1,1)
+    private void MoveTo()
+    {
+        Vector2 velocity = Vector2.one * playerSpeed;
+        Debug.Log(velocity.magnitude);
+        playerRigidbody.velocity = velocity;
+    }
+}
