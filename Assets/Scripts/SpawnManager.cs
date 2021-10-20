2516875705f2d9d23418c36c0426cbb287327040
habibi55 <muhamadhabibi14168@gmail.com>
2021-10-20 17:27:04 +0700

    problem-6

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject littleBoxPrefab;
+
+    private void Start()
+    {
+        RandomSpawnBox();
+    }
+
+    private void RandomSpawnBox()
+    {
+        int numberOfBox = Random.Range(1, 10);
+
+        for (int i = 0; i < numberOfBox; i++)
+        {
+            Vector2 randomPosition = RandomizePosition();
+
+            Instantiate(littleBoxPrefab, randomPosition, littleBoxPrefab.transform.rotation);
+        }
+    }
+
+    private Vector2 RandomizePosition()
+    {
+        float randomXAxis = Random.Range(-2.75f, 2.75f);
+        float randomYAxis = Random.Range(-2.75f, 2.75f);
+
+        return new Vector2(randomXAxis, randomYAxis);
+    }
+}
